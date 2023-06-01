f6264dd5f752811cff2085d81dc717d45c13cfc6
pauft <1195566313@qq.com>
2023-06-01 21:14:30 +0800

    init

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+        {{a}}
+
+        <div>
+          {{ hello }}
+        </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const hello = ref('hello')
+const a = 1
+</script>
